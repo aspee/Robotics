6bb5674f69e8ffc3e2f65cc44acaac5331fd9dc2
Sushant Parab <sushantwar157@gmail.com>
2019-01-29 17:26:56 +0530

    Base added

---
+++ sprites/Main.java
@@ -0,0 +1,32 @@
+import ch.aplu.robotsim.LegoRobot;
+import ch.aplu.robotsim.Gear;
+import ch.aplu.robotsim.Tools;
+public class Main {
+ Main () {
+    LegoRobot robot = new LegoRobot();
+    Gear gear = new Gear();
+    robot.addPart(gear);                                
+    gear.forward();
+    Tools.delay(2000);
+    gear.left(550);
+    gear.forward();
+    Tools.delay(2000);
+    gear.left(550);
+    gear.forward();
+    Tools.delay(2000);
+    gear.left(550);
+    gear.forward();
+    Tools.delay(2000);
+    gear.right(550);
+    gear.forward();
+    Tools.delay(2000);
+    }
+  public static void main(String[] args) {
+    new Main();
+    }
+  }
+  
+  
+  
+    
+  
